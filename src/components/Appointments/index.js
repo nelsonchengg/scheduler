a70ecf58178201bc22285ad31d080c024bc28d8e
nelsonchengg <nelsoncheng103@gmail.com>
2023-04-20 19:17:41 -0400

    created more components and stories

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "components/Appointments/styles.scss"
+
+export default function Appointment() {
+  return (
+    <article
+      className="appointment">
+    </article>
+  );
+}
